4eb15b954e4849926a383122e89e3e6ab74f5a8e
Danny Choi <choi.danny20@gmail.com>
2020-08-01 20:54:33 -0500

    deleted unnecessary function

--- searchCity.js
+++ searchCity.js
@@ -2,14 +2,6 @@
 let $citySearch = $("#citySearch");
 // let $cityList = $("#cityList");
 
-// $searchBtn.on("click", () => {
-//   event.preventDefault();
-//   $(`<ul>
-//     <li>${$citySearch.val()}</li>
-//   </ul>`).appendTo($cityList);
-//   $citySearch.val("");
-// });
-
 function cityNameSearch() {
   let cityName = $("#citySearch").val();
 
